fc38a8c22d4f437f85784189db04308fd4872b71
someone <someone@someplace.com>
2018-11-24 18:33:33 -0800

    added backend for react todo

--- todo/todolist/models.py
+++ todo/todolist/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Todo(models.Model):
 	title = models.CharField(max_length=25),
 	body = models.TextField(null=True, blank=True),
+	checked = models.BooleanField(),
 
 def __str__(self):
     return self.title
